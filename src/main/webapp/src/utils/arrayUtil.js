4a3912bdad561933ec08ab9e44d90df03a850d2e
CoolMcCool <tom.weber@rodenstock.com>
2015-10-15 11:27:11 +0200

    Added comments and implemented test for mkString

--- src/main/webapp/src/utils/arrayUtil.js
+++ src/main/webapp/src/utils/arrayUtil.js
@@ -1,12 +1,25 @@
 export default {
+  /**
+   * Checks whether the defined object is an array or not.
+   * @param {object} obj the defined object.
+   * @return {boolean} true if the defined object is an array.
+   */
   isArray(obj) {
     return Object.prototype.toString.call(obj) === '[object Array]';
   },
 
+  /**
+   * Makes a string defined by an array, a separator, a prefixed string,
+   * and a suffixed string.
+   * @param {array} array the defined array of elements.
+   * @param {string} separator the defined separator.
+   * @param {string} before the defined prefixed string.
+   * @param {string} after the defined suffixed string.
+   * @return {string} a string representation of the defined parameters.
+   */
   mkString(array, separator = ', ', before = '', after = '') {
     return array.reduce((result, item, index) => {
         let newResult = result;
-
         if (index > 0) {
           newResult = newResult + separator;
         }
