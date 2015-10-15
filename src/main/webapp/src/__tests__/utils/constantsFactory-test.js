9a1795dc34d9f857715938f79f20994cde6f97f7
Michael Wellner <michael.wellner@de.ibm.com>
2015-10-15 11:01:26 +0200

    Added arrayUtils.

--- src/main/webapp/src/__tests__/utils/constantsFactory-test.js
+++ src/main/webapp/src/__tests__/utils/constantsFactory-test.js
@@ -1,9 +1,18 @@
 jest.dontMock('../../utils/constantsFactory');
 
 describe('constantsFactory', function() {
-  it('creates an object with constants from a list of strings', function() {
+  it('creates an object with constants from a list of strings (array)', function() {
     const constantsFactory = require('../../utils/constantsFactory');
     expect(constantsFactory([ 'A', 'B', 'C' ])).toEqual({
+      A: 'A',
+      B: 'B',
+      C: 'C'
+    });
+  });
+
+  it('creates an object with constants from list of strings (arguments)', function() {
+    const constantsFactory = require('../../utils/constantsFactory');
+    expect(constantsFactory('A', 'B', 'C')).toEqual({
       A: 'A',
       B: 'B',
       C: 'C'
--- src/main/webapp/src/utils/webservice.js
+++ src/main/webapp/src/utils/webservice.js
@@ -1,7 +1,12 @@
 import { Client } from 'node-rest-client';
 import URLUtil from './URLUtil';
+import ConstantsFactory from './constantsFactory';
 
 export default class WebServiceClient {
+
+  static get ORDER_BY() {
+    return ConstantsFactory('ASC', 'DESC');
+  }
 
   constructor(servicePath) {
     const serviceURL = `${URLUtil.baseURL()}/${servicePath}`;
@@ -17,6 +22,37 @@ export default class WebServiceClient {
   }
 
   onError(errorHandler) {
-    this.errorHandler();
+    this.client.on('error', function(err) {
+      errorHandler(errorHandler);
+    });
+  }
+
+  /**
+
+   * @param {number} start is the first delivered entities.
+   * @param {number} count is the number of delivered entities.
+   * @param {string} orderBy is the name of the column the entities should be ordered. An array of strings will be transformed to a comma-speperated string.
+   * @param {boolean} asc identifies the sorting order, default is true.
+   * @param {object} where is a map where you define options to filter your entities. E.g. { "foo": "*bar" }
+   * @returns {array} a list of JSON entities.
+   */
+  list(start = 0, count = 0, orderBy = '', asc = true, where = {}) {
+    const data = {};
+
+    if (start !== 0) {
+      data[start] = start;
+    }
+
+    if (count !== 0) {
+      data[count] = count;
+    } else if (start !== 0) {
+      data[count] = 100;
+    }
+
+    if (orderBy.length > 0) {
+
+    }
+
+    this.client.methods.list(data);
   }
 }
